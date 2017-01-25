53e0235cc8ce776fb50a57d1e6e6b5dcc4127b48
Noriko Ohnuki <nnorky@gmail.com>
2017-01-25 01:39:05 +0000

    using Bootstrap

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,5 +2,19 @@ class MessagesController < ApplicationController
   def index
     #render 'index'
     # views/messages/index.html.erb を呼び出す
+    @message = Message.new
+     # Messageを全て取得する。
+    @messages = Message.all
   end
+ def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました'
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:name, :body)
+  end
+  ## ここまで
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   #get 'messages/index', to: 'messages#index'
   root 'messages#index'
-
+  resources :messages , only: [:create]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
