cf5c027fa24c80d06ff93aa28051bc578a6f36c0
Noriko Ohnuki <nnorky@gmail.com>
2017-01-26 03:41:48 +0000

    added deletion link and notice

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+  
   def index
     #render 'index'
     # views/messages/index.html.erb を呼び出す
@@ -6,15 +8,37 @@ class MessagesController < ApplicationController
      # Messageを全て取得する。
     @messages = Message.all
   end
- def create
+  
+  def create
     @message = Message.new(message_params)
     @message.save
     redirect_to root_path , notice: 'メッセージを保存しました'
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
   end
 
   private
   def message_params
     params.require(:message).permit(:name, :body)
   end
-  ## ここまで
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
 end
